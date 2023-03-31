d09a1a397aa9590e2a6f14137735d93f4a75b871
iwatanabee <issei.watanabe0708@gmail.com>
2023-03-31 13:41:57 +0900

    fix android version

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -36,13 +36,6 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
         ],
-      //   title: Center(
-      //     child: const Text(
-      //       '食堂１',
-      //       style: TextStyle(color: Colors.white),
-      //     ),
-      //   ),
-      //   backgroundColor: Colors.pink.shade50,
       ),
       drawer: Drawer(
         child: ListView(
@@ -65,39 +58,6 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
 
-      // body: Center(
-      //   child: ListView(
-      //     children: <Widget>[
-      //       Text(
-      //         '予想待ち時間 XX分',
-      //         style: TextStyle(
-      //           fontSize: 30,
-      //           color: Colors.lightBlueAccent,
-      //           fontWeight: FontWeight.bold,
-
-      //         ),
-      //         textAlign: TextAlign.center,
-      //       ),
-      //       Padding(
-      //         padding: const EdgeInsets.all(10.0),
-      //         child: Column(
-      //           children: [
-      //             Container(
-      //               child: const Text(
-      //                 '人数　○○人',
-      //                 style: TextStyle(
-      //                   fontSize: 20,
-      //                   color: Colors.lightBlueAccent,
-      //                 ),
-      //                 textAlign: TextAlign.center,
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
       body: Center(
         child: GridView.count(
           crossAxisCount: 2,
